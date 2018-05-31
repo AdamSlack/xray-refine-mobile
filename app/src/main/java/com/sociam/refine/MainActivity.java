f76cc10c9d6cf7822d26b727f1b8a2119cc40a27
Adam Slack <adamslack@outlook.com>
2018-05-31 21:49:42 +0100

    some form of drawer in place, need to sort menu items for it out

--- app/src/main/java/com/sociam/refine/MainActivity.java
+++ app/src/main/java/com/sociam/refine/MainActivity.java
@@ -9,12 +9,16 @@ import android.os.Bundle;
 import android.os.Process;
 import android.provider.Settings;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Toolbar;
+import android.widget.ListView;
 
 import java.util.Calendar;
 import java.util.Map;
@@ -23,29 +27,72 @@ import static android.app.AppOpsManager.MODE_ALLOWED;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout mDrawer;
-    private android.support.v7.widget.Toolbar toolbar;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button viewAppBtn = (Button) findViewById(R.id.viewAppsBtn);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        viewAppBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent findOwnApps = new Intent(getApplicationContext(), ListApplications.class);
-                startActivity(findOwnApps);
-            }
-        });
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        mDrawerLayout.addDrawerListener(
+                new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                        // Respond when the drawer's position changes
+                    }
+
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        // Respond when the drawer is opened
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        // Respond when the drawer is closed
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        // Respond when the drawer motion state changes
+                    }
+                }
+        );
+
+//        Button viewAppBtn = (Button) findViewById(R.id.viewAppsBtn);
+//
+//        viewAppBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent findOwnApps = new Intent(getApplicationContext(), ListApplications.class);
+//                startActivity(findOwnApps);
+//            }
+//        });
 
         if (!MainActivity.checkForPermission(this)) {
             startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
         }
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     static public boolean checkForPermission(Context context) {
         AppOpsManager appOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);

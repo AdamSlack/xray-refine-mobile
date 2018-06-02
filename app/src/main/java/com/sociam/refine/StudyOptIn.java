dab4ff394523747cde9e11a4a62374bee7d5c778
Adam Slack <adamslack@outlook.com>
2018-06-02 20:11:47 +0100

    study opt in form

---
+++ app/src/main/java/com/sociam/refine/StudyOptIn.java
@@ -0,0 +1,13 @@
+package com.sociam.refine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudyOptIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_opt_in);
+    }
+}

4ce0aa39c80e993b7458cae9bb739901bb1929a6
adam slack <adamslack@outlook.com>
2018-06-22 11:49:15 +0100

    XRay CSM Adapter and JSON Parser

---
+++ app/src/main/java/com/sociam/refine/XRayCSMApp.java
@@ -0,0 +1,13 @@
+package com.sociam.refine;
+
+public class XRayCSMApp {
+    public int id;
+    public String name;
+    public String ageRating;
+    public int CSMRating;
+    public String oneLiner;
+    public String CSMURI;
+    public String playStoreURL;
+    public String appPackageName;
+    public XRayCSMParentalGuidance parental_guidances;
+}
---
+++ app/src/main/java/com/sociam/refine/XRayCSMParentalGuidance.java
@@ -0,0 +1,16 @@
+package com.sociam.refine;
+
+public class XRayCSMParentalGuidance {
+    public Guidance playability = new Guidance();
+    public Guidance violence = new Guidance();
+    public Guidance sex = new Guidance();
+    public Guidance language = new Guidance();
+    public Guidance consumerism = new Guidance();
+    public Guidance drugs = new Guidance();
+    public Guidance educational = new Guidance();
+
+    public static class Guidance {
+        public int rating = 0;
+        public String description = "No Rating";
+    }
+}

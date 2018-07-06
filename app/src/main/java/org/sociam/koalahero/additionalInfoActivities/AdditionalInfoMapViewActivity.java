8ab48067b26138e18500f8d0d3aaf440daa8e873
Thomas Burton <twyburton@outlook.com>
2018-07-06 13:46:45 +0100

    Change mapview to geochart/webview

--- app/src/main/java/org/sociam/koalahero/additionalInfoActivities/AdditionalInfoMapViewActivity.java
+++ app/src/main/java/org/sociam/koalahero/additionalInfoActivities/AdditionalInfoMapViewActivity.java
@@ -5,6 +5,7 @@ import android.os.LocaleList;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.webkit.WebView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -28,13 +29,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
 import java.util.Locale;
+import java.util.Map;
 
-public class AdditionalInfoMapViewActivity extends AppCompatActivity implements OnMapReadyCallback{
-    private MapView mapView;
-    private GoogleMap gmap;
-    private GeoJsonLayer layer;
-
-    private static final String MAP_VIEW_BUNDLE_KEY = "AIzaSyBsrb-XjGWafuOr61to_VcnBoe1--JDyDk";
+public class AdditionalInfoMapViewActivity extends AppCompatActivity{
+//    private MapView mapView;
+//    private GoogleMap gmap;
+//    private GeoJsonLayer layer;
+//
+//    private static final String MAP_VIEW_BUNDLE_KEY = "AIzaSyBsrb-XjGWafuOr61to_VcnBoe1--JDyDk";
 
     private CountryCodeConverter ccConverter;
     private String packageName;
@@ -58,105 +60,132 @@ public class AdditionalInfoMapViewActivity extends AppCompatActivity implements 
         getSupportActionBar().setTitle("Where In The World");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        Bundle mapViewBundle = null;
-        if (savedInstanceState != null) {
-            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+//        Bundle mapViewBundle = null;
+//        if (savedInstanceState != null) {
+//            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+//        }
+//
+//        this.mapView = findViewById(R.id.mapView);
+//        this.mapView.onCreate(mapViewBundle);
+//        this.mapView.getMapAsync(this);
+
+
+        String rows = "";
+        int n = 0;
+
+        Map<String,Integer> countryCodeCounts = app.localeCounts;
+
+        for (String code: countryCodeCounts.keySet()){
+            //['Germany', 200]
+
+            if( code != null && code != "-99") {
+                if (n > 0) rows += ",";
+                rows += "['" + ccConverter.iso2CountryCodeToName(code.toUpperCase()) + "'," + countryCodeCounts.get(code) + "]";
+                n++;
+            }
         }
 
-        this.mapView = findViewById(R.id.mapView);
-        this.mapView.onCreate(mapViewBundle);
-        this.mapView.getMapAsync(this);
+        String data = "";
+
+        data += "<html><head><script type='text/javascript' src='https://www.google.com/jsapi'></script> <script type='text/javascript'> google.load('visualization', '1', {'packages': ['geochart']});google.setOnLoadCallback(drawRegionsMap);";
+        data += " function drawRegionsMap() { var data = new google.visualization.DataTable();data.addColumn('string', 'Country');data.addColumn('number', 'Number of Hosts');data.addRows([" + rows + "]);";
+        data += "var options = {};var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));chart.draw(data, options); };</script></head><body style='padding: 10px 0;'><div id='chart_div' style='width: 100%;'></div></body></html> ";
+
+
+        WebView wv = (WebView) findViewById(R.id.map_view);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadDataWithBaseURL("", data, "text/html", "UTF-8", "");
 
 
     }
 
 //
 
-    @Override
-    public void onMapReady(GoogleMap map) {
-        this.gmap = map;
-        try {
-
-            // create empty layer. I can't find out how to make an empty geo json layer :/
-            this.layer = new GeoJsonLayer(gmap, R.raw.country_data, getApplicationContext());
-            ArrayList<GeoJsonFeature> features = new ArrayList<>();
-            for(GeoJsonFeature f : this.layer.getFeatures()) {
-                GeoJsonPolygonStyle s = new GeoJsonPolygonStyle();
-                s.setFillColor(0x00000000);
-                s.setStrokeColor(0x00000000);
-                f.setPolygonStyle(s);
-                features.add(f);
-            }
-            for(GeoJsonFeature f : features) {
-                this.layer.removeFeature(f);
-            }
-
-            for(GeoJsonFeature f : features) {
-                String code = f.getId();
-                String twoISO = ccConverter.iso3CountryCodeToIso2CountryCode(code);
-                System.out.println(code + " -- " + twoISO);
-
-                if(twoISO != null && app.localeCounts.containsKey(twoISO)) {
-                    System.out.println("\n---------\n---------\n--------\n--------\n---------\n---------\n---------\nIt's a Match! " + twoISO);
-
-                    GeoJsonPolygonStyle style = new GeoJsonPolygonStyle();
-                    style.setFillColor(getApplicationContext().getResources().getColor(R.color.colorRed));
-                    style.setStrokeColor(getApplicationContext().getResources().getColor(R.color.colorRedDark));
-                    f.setPolygonStyle(style);
-
-                    this.layer.addFeature(f);
-                }
-
-            }
-
-            layer.addLayerToMap();
-        }
-        catch (IOException err) {
-            err.printStackTrace();
-        }
-        catch (JSONException err) {
-            err.printStackTrace();
-        }
-        catch (ConcurrentModificationException err) {
-            err.printStackTrace();
-        }
-        gmap.setMinZoomPreference(1.0f);
-        gmap.setMaxZoomPreference(21.0f);
-        LatLng ny = new LatLng(51.5074, 0.1278);
-        gmap.moveCamera(CameraUpdateFactory.newLatLng(ny));
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mapView.onResume();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mapView.onStart();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mapView.onStop();
-    }
-    @Override
-    protected void onPause() {
-        mapView.onPause();
-        super.onPause();
-    }
-    @Override
-    protected void onDestroy() {
-        mapView.onDestroy();
-        super.onDestroy();
-    }
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
+//    @Override
+//    public void onMapReady(GoogleMap map) {
+//        this.gmap = map;
+//        try {
+//
+//            // create empty layer. I can't find out how to make an empty geo json layer :/
+//            this.layer = new GeoJsonLayer(gmap, R.raw.country_data, getApplicationContext());
+//            ArrayList<GeoJsonFeature> features = new ArrayList<>();
+//            for(GeoJsonFeature f : this.layer.getFeatures()) {
+//                GeoJsonPolygonStyle s = new GeoJsonPolygonStyle();
+//                s.setFillColor(0x00000000);
+//                s.setStrokeColor(0x00000000);
+//                f.setPolygonStyle(s);
+//                features.add(f);
+//            }
+//            for(GeoJsonFeature f : features) {
+//                this.layer.removeFeature(f);
+//            }
+//
+//            for(GeoJsonFeature f : features) {
+//                String code = f.getId();
+//                String twoISO = ccConverter.iso3CountryCodeToIso2CountryCode(code);
+//                System.out.println(code + " -- " + twoISO);
+//
+//                if(twoISO != null && app.localeCounts.containsKey(twoISO)) {
+//                    System.out.println("\n---------\n---------\n--------\n--------\n---------\n---------\n---------\nIt's a Match! " + twoISO);
+//
+//                    GeoJsonPolygonStyle style = new GeoJsonPolygonStyle();
+//                    style.setFillColor(getApplicationContext().getResources().getColor(R.color.colorRed));
+//                    style.setStrokeColor(getApplicationContext().getResources().getColor(R.color.colorRedDark));
+//                    f.setPolygonStyle(style);
+//
+//                    this.layer.addFeature(f);
+//                }
+//
+//            }
+//
+//            layer.addLayerToMap();
+//        }
+//        catch (IOException err) {
+//            err.printStackTrace();
+//        }
+//        catch (JSONException err) {
+//            err.printStackTrace();
+//        }
+//        catch (ConcurrentModificationException err) {
+//            err.printStackTrace();
+//        }
+//        gmap.setMinZoomPreference(1.0f);
+//        gmap.setMaxZoomPreference(21.0f);
+//        LatLng ny = new LatLng(51.5074, 0.1278);
+//        gmap.moveCamera(CameraUpdateFactory.newLatLng(ny));
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        mapView.onResume();
+//    }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        mapView.onStart();
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        mapView.onStop();
+//    }
+//    @Override
+//    protected void onPause() {
+//        mapView.onPause();
+//        super.onPause();
+//    }
+//    @Override
+//    protected void onDestroy() {
+//        mapView.onDestroy();
+//        super.onDestroy();
+//    }
+//    @Override
+//    public void onLowMemory() {
+//        super.onLowMemory();
+//        mapView.onLowMemory();
+//    }
 
 }
